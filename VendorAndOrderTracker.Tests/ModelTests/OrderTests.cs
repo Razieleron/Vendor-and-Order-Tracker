2d3881c5074b5c96dc3ac0011774a1f007bca131
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-03 13:13:31 -0800

    add test to return Order Name

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -6,13 +6,22 @@ using System;
 namespace Orders.Tests 
 {
   [TestClass]
-  public class TrackTests
+  public class OrderTests
   {
     [TestMethod]
-    public void TrackConstructor_CreateInstanceOfTrack_Track()
+    public void OrderConstructor_CreateInstanceOfOrder_Order()
     {
       Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+
+    public void GetOrderName_ReturnOrderName_String()
+    {
+      string orderName = "Frozen";
+      Order newOrder = new Order(orderName);
+      string result = newOrder.OrderName;
+      Assert.AreEqual(orderName, result);
     }
   }
 }
