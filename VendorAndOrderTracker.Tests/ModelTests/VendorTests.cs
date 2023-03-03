4f2a154b42d9937c04c6735f8437ce7229bc38dc
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-03 12:52:11 -0800

    pass second Vendor test

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,13 +6,21 @@ using System;
 namespace VendorAndOrderTracker.Tests
 {
   [TestClass]
-  public class VendorTests
-  {
-  [TestMethod]
-  public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
-  {
-    Vendor newVendor = new Vendor("Maria's Muffins");
-    Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-  }
+    public class VendorTests
+    {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Maria's Muffins");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void GetName_ReturnsVendorName_String()
+    {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
+    }
   }
 } 
